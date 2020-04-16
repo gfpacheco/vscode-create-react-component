d32dca03ac9d0c250de65a5892d5e008a3c909ea
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-04-15 21:00:16 -0300

    Add create react native command

--- extension.js
+++ extension.js
@@ -3,41 +3,49 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const componentTemplatePath = path.join(__dirname, 'templates', 'component.template');
+const nativeComponentTemplatePath = path.join(__dirname, 'templates', 'nativeComponent.template');
 const indexTemplatePath = path.join(__dirname, 'templates', 'index.template');
 
 function activate(context) {
-  const disposable = vscode.commands.registerCommand(
-    'create-react-component.createComponent',
-    async function (uri) {
-      const componentName = await vscode.window.showInputBox({ prompt: 'Component name' });
+  async function createComponent(uri, templatePath) {
+    const componentName = await vscode.window.showInputBox({ prompt: 'Component name' });
 
-      if (!componentName) {
-        return;
-      }
+    if (!componentName) {
+      return;
+    }
 
-      const parentFolder = uri ? uri.fsPath : vscode.workspace.rootPath;
-      const componentFolder = path.join(parentFolder, componentName);
+    const parentFolder = uri ? uri.fsPath : vscode.workspace.rootPath;
+    const componentFolder = path.join(parentFolder, componentName);
 
-      try {
-        await fs.mkdir(componentFolder);
+    try {
+      await fs.mkdir(componentFolder);
 
-        const [componentTemplate, indexTemplate] = await Promise.all([
-          fs.readFile(componentTemplatePath),
-          fs.readFile(indexTemplatePath),
-        ]);
+      const [componentTemplate, indexTemplate] = await Promise.all([
+        fs.readFile(templatePath),
+        fs.readFile(indexTemplatePath),
+      ]);
 
-        const component = componentTemplate.toString().replace(/{componentName}/g, componentName);
-        const index = indexTemplate.toString().replace(/{componentName}/g, componentName);
+      const component = componentTemplate.toString().replace(/{componentName}/g, componentName);
+      const index = indexTemplate.toString().replace(/{componentName}/g, componentName);
 
-        await fs.writeFile(path.join(componentFolder, `${componentName}.js`), component);
-        await fs.writeFile(path.join(componentFolder, `index.js`), index);
-      } catch (error) {
-        console.error(error);
-      }
-    },
+      await fs.writeFile(path.join(componentFolder, `${componentName}.js`), component);
+      await fs.writeFile(path.join(componentFolder, `index.js`), index);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand('create-react-component.createComponent', (uri) =>
+      createComponent(uri, componentTemplatePath),
+    ),
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand('create-react-component.createNativeComponent', (uri) =>
+      createComponent(uri, nativeComponentTemplatePath),
+    ),
+  );
 }
 exports.activate = activate;
 
